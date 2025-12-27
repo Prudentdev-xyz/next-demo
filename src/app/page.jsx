c581d1ad7a51a952c08d57307f21cc746ba105f8
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-12-27 15:31:30 -0800

    demo-learning

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+         <div className='p-10 text-black'>page</div>
+         <h1 className='p-10 text-black text-3xl font-bold'>Wwelcome Home!</h1>
+    </div>
+  )
+}
+
+export default page;
+
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const page = () => {
+  return (
+    <div className='text-3xl text-black'>page</div>
+  )
+}
+
+export default page
